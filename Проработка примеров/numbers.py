5c759f80580dc18c988bb309f1807837311432b8
2302Nikolay <Malzewn@gmail.com>
2022-05-22 18:20:25 +0300

    Третий пример

---
+++ Проработка примеров/numbers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+if __name__ == '__main__':
+
+    UsersNum1 = int(input("Введите 4 числа: \n"))
+    UsersNum2 = int(input())
+    UsersNum3 = int(input())
+    UsersNum4 = int(input())
+
+    sum1 = UsersNum1 + UsersNum2
+    sum2 = UsersNum3 + UsersNum4
+    x = sum1/sum2
+
+    print('%.2f' % x)

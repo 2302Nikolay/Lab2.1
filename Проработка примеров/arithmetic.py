834f676b3210e1d336d0de9cea18930167d2b2f8
2302Nikolay <Malzewn@gmail.com>
2022-05-22 18:18:15 +0300

    Первый пример

---
+++ Проработка примеров/arithmetic.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+if __name__ == '__main__':
+
+    UserAnswer = input("Попробуйте решить пример: 4*100-54\n Введите ответ: ")
+    print("\nВаш ответ: ", UserAnswer, "\nПравильный ответ: ", 4*100-54)

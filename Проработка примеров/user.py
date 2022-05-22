c3bdde52e699bfb10322898920f9353c128dd825
2302Nikolay <Malzewn@gmail.com>
2022-05-22 18:19:15 +0300

    Второй пример

---
+++ Проработка примеров/user.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+if __name__ == '__main__':
+
+    name = input("What is your name? - ")
+    old = input("How old are you? - ")
+    city = input("Where are you live? - ")
+    print("This is ", name, "\nIt is ", old, "\n(S)he live in ", city)
